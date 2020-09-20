b60672e569be4b9db808f23ba5b78a58a8055579
LemeshkoNastya <anastasia.lemeshko23@gmail.com>
2020-09-20 16:57:44 +0400

    init: star web-application

---
+++ js/script.js
@@ -0,0 +1,44 @@
+//Tub
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    let tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    //скрытие ненужных табов
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    //показывает табы
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    //делегирование событий, нажначение клика
+    tabsParent.addEventListener('click', function(event) {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
